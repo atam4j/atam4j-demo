712e3bfb14a53b72163336ae6c5cc2561d7a8846
Phill <phillbarber>
2019-05-06 16:27:07 +0100

    add showpopup method

--- planes-4-sale-site/src/main/resources/assets/js/searchResults.js
+++ planes-4-sale-site/src/main/resources/assets/js/searchResults.js
@@ -1,19 +1,24 @@
 function SearchResultsViewModel() {
     var self = this;
 
-    self.planes = ko.observable()
+    self.planes = ko.observableArray()
+
+    self.showPopup = function (){
+        console.log("THE REG IS " + this.reg);
+        console.log("THE ID IS " + this.id);
+        this.popupDisplayed(true);
+    };
 
     $.get( "/api/search?manufacturer=boeing", function( data ) {
-      console.log(data)
-      _.each(data, function(plane){
+
+      self.planes(_.map(data, function(plane ){
         plane.imageURL="/plane-photos/" + plane.imageId + ".jpg";
-        console.log("WOOOO")
-      } )
-      console.log("DONE")
-
-      self.planes(data);
+        plane.popupDisplayed = ko.observable(false);
+        var thePlane = ko.observable(plane);
+        return thePlane;
+      }
+      ));
     });
-
 }
 
 ko.applyBindings(new SearchResultsViewModel());

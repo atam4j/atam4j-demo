550e761634492f9e07a67a7bad858df0cd563c13
phillbarber <phillbarber>
2019-05-11 16:53:22 +0100

    Make Search Acceptance test NOT a @Monitor test

--- planes-4-sale-site/src/test/java/me/atam/planes4sale/browser/SearchAcceptanceTest.java
+++ planes-4-sale-site/src/test/java/me/atam/planes4sale/browser/SearchAcceptanceTest.java
@@ -7,14 +7,13 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@Monitor
 public class SearchAcceptanceTest extends BrowserBasedAcceptanceTest {
 
     @Test
     public void canSearchForBoeings() {
         SearchResultsPage searchResultsPage = SearchResultsPage.load(getHostAndPort(), "boeing", driver);
 
-        SearchResultsPage.PlaneView planeView = searchResultsPage.getPlaneView("plane-1");
+        SearchResultsPage.PlaneView planeView = searchResultsPage.getPlaneView("plane-0");
         assertThat(planeView.isDisplayed(), is(true));
         assertThat(planeView.getManufacturer(), is("Boeing"));
         assertThat(planeView.getModel(), is("777-31B/ER"));

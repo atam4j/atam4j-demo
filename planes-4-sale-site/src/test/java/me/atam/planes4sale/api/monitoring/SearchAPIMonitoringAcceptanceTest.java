d8015c12c87e210d1c42009a8f7c585609ba3c72
phillbarber <phillbarber>
2019-05-11 16:55:17 +0100

    Add @Monitor test for search api

---
+++ planes-4-sale-site/src/test/java/me/atam/planes4sale/api/monitoring/SearchAPIMonitoringAcceptanceTest.java
@@ -0,0 +1,59 @@
+package me.atam.planes4sale.api.monitoring;
+
+import me.atam.atam4j.Monitor;
+import me.atam.planes4sale.APIClient;
+import me.atam.planes4sale.AcceptanceTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+@Monitor
+public class SearchAPIMonitoringAcceptanceTest extends AcceptanceTest {
+
+    private APIClient apiClient = new APIClient(getHostAndPort());
+
+    @Test
+    public void canSearchForBoeings() {
+        Response apiResponse = apiClient.getSearchResultsFor("boeing");
+
+        assertThat(apiResponse.getStatus(), is(200));
+        List<Map<String,Object>> planes = apiResponse.readEntity(List.class);
+        assertThat(planes.size(), is(greaterThan(0)));
+        Map<String, Object> plane = planes.get(0);
+        assertThat(plane.get("manufacturer"), is("Boeing"));
+        checkHasAttributeWithNonNullStringValue(plane ,"manufacturer");
+        checkHasAttributeWithNonNullStringValue(plane ,"id");
+        checkHasAttributeWithNonNullStringValue(plane ,"imageId");
+        checkHasAttributeWithNonNullStringValue(plane ,"reg");
+        checkHasAttributeWithNonNullStringValue(plane ,"manufactureDate");
+    }
+
+    @Test
+    public void canSearchForAirbus() {
+        Response apiResponse = apiClient.getSearchResultsFor("airbus");
+
+        assertThat(apiResponse.getStatus(), is(200));
+        List<Map<String,Object>> planes = apiResponse.readEntity(List.class);
+        assertThat(planes.size(), is(greaterThan(0)));
+        Map<String, Object> plane = planes.get(0);
+        assertThat(plane.get("manufacturer"), is("Airbus"));
+        checkHasAttributeWithNonNullStringValue(plane ,"manufacturer");
+        checkHasAttributeWithNonNullStringValue(plane ,"id");
+        checkHasAttributeWithNonNullStringValue(plane ,"imageId");
+        checkHasAttributeWithNonNullStringValue(plane ,"reg");
+        checkHasAttributeWithNonNullStringValue(plane ,"manufactureDate");
+    }
+
+
+    private void checkHasAttributeWithNonNullStringValue(Map<String, Object> plane, String attribute) {
+        assertThat(plane.get(attribute), is(notNullValue()));
+        assertThat(plane.get(attribute), instanceOf(String.class));
+        assertThat(((String) plane.get(attribute)).length(), greaterThan(1 ));
+    }
+}
